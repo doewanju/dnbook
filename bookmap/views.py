4e165dd2065f838d8ce681bde89719eb88c32811
Haesun-Pyeon <sme1357@naver.com>
2019-10-09 19:59:37 +0900

    한글날한거

--- bookmap/views.py
+++ bookmap/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import BookStore
 from django.core import serializers
+from .review import reviewFuc
 import simplejson
 
 # Create your views here.
@@ -11,7 +12,8 @@ def bookstore(request):
 
 def detail(request, bookstore_id):
     store_detail = get_object_or_404(BookStore, pk = bookstore_id)
-    return render(request, 'storedetail.html', {'store' : store_detail})
+    rev=reviewFuc(store_detail.name,store_detail.addr)
+    return render(request, 'storedetail.html', {'store' : store_detail, 'rev':rev})
 
 def realmap(request):
     bookstores = BookStore.objects.all()
