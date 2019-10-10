47fcd4ef6d3c3f625a5d07380e9b5278eab2c900
Haesun-Pyeon <sme1357@naver.com>
2019-10-10 15:54:08 +0900

    e편한세상 고침

--- bookmap/review.py
+++ bookmap/review.py
@@ -2,11 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
 from webdrivermanager import ChromeDriverManager as ch
-'''
-만약 네이버에 등록된 곳이면 그 곳 블로그리뷰 클릭해서 리뷰가져오고
-등록안된곳이면 지금 하는거처럼 블로그 상단 세건 가져오도록
-만약 등록된곳 가져올수있으면 영업시간도 가져오기
-'''
+from selenium.common.exceptions import NoSuchElementException
 
 def reviewFuc(name, addr):
     find_name = name
@@ -25,21 +21,36 @@ def reviewFuc(name, addr):
     naver_url="https://www.naver.com"
     driver.get(naver_url)
 
+    tf=False
     driver.find_element_by_class_name('input_text').clear()
     driver.find_element_by_class_name('input_text').send_keys(f)
     driver.find_element_by_class_name('sch_smit').click()
-    driver.find_element_by_class_name('lnb3').click()
+    try:
+        driver.find_element_by_class_name('review').click()
+    except NoSuchElementException:
+        driver.find_element_by_class_name('lnb3').click()
+        tf=True
     driver.switch_to.window(driver.window_handles[-1])
     url=driver.current_url
 
+    res=[]
     page=requests.get(url)
     soup=BeautifulSoup(page.content,'html.parser')
-    li=soup.find_all('li','sh_blog_top')
-    res=[]
-    for i in range(3):
-        title=li[i].find('a','sh_blog_title').get_text()
-        content=li[i].find('dd','sh_blog_passage').get_text()
-        link=li[i].find('a','sh_blog_title').get('href')
-        inf={'title':title, 'content':content, 'link':link}
-        res.append(inf)
+    if (tf==True):
+        li=soup.find_all('li','sh_blog_top')
+        for i in range(3):
+            title=li[i].find('a','sh_blog_title').get_text()
+            content=li[i].find('dd','sh_blog_passage').get_text()
+            link=li[i].find('a','sh_blog_title').get('href')
+            inf={'title':title, 'content':content, 'link':link}
+            res.append(inf)
+    else:
+        li=soup.find_all('li','type_review')
+        for i in range(5):
+            title=li[i].find('a','name').get_text()
+            content=li[i].find('div','ellp2').get_text()
+            link=li[i].find('a','name').get_text()
+            inf={'title':title, 'content':content, 'link':link}
+            res.append(inf)
+
     return res
