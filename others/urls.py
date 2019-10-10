b2a712ccd0239e86dbd6e2d53796d9053abd67c4
ehdhkswn1997 <ehdhkswn@naver.com>
2019-10-10 18:20:34 +0900

    문화프로그램 댓글기능

--- others/urls.py
+++ others/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('board/', views.board, name='board'),
     path('guide/', views.guide, name='guide'),
     path('detail/<int:culture_id>', views.detail, name='culturedetail'),
+    path('commentcreate/<int:culture_id>', views.commentcreate, name='commentcreate'),
+    path('commentdelete/<int:comment_id>', views.commentdelete, name='commentdelete'),
 ]
