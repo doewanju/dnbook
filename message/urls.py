37fc8d5f58cffd6323937f4a7f4594bc2a944ed5
ehdhkswn1997 <ehdhkswn@naver.com>
2019-10-30 01:19:08 +0900

    chat

--- message/urls.py
+++ message/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('listmessage/', views.listMessage, name='listMessage'),
     path('detail/<int:message_id>', views.viewMessage, name='viewMessage'),
     path('send/<int:user_id>', views.sendMessage, name='sendMessage'),
+    path('chat/<int:user_id>', views.chat, name='chat'),
 ]
