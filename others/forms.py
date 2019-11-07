eab21979c49d14fc73d862a379bfa091ebc19c06
EHWAGABI <twoflowersj@likelion.org>
2019-11-07 23:00:25 +0900

    문화detail 조금남음

--- others/forms.py
+++ others/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Comment, Culture
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder':' 댓글을 입력해주세요.'}), label='')
+
     class Meta:
         model = Comment
         fields = ['content']
