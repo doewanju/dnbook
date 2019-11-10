b42f74f9254f755554f4067336681f25200fb55e
yuna <yunaeunh@choejiyun-ui-MacBookPro.local>
2019-11-11 01:58:33 +0900

    culturedetail 완성

--- others/forms.py
+++ others/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Comment, Culture
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder':' 댓글 내용을 입력해주세요.'}), label='')
     class Meta:
         model = Comment
         fields = ['content']
