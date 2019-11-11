a920e91625b7fc74a49e506c1cf4cb986af25dc4
yuna <yunaeunh@choejiyun-ui-MacBookPro.local>
2019-11-11 17:22:56 +0900

    글쓰기 페이지 완성

--- others/forms.py
+++ others/forms.py
@@ -8,6 +8,8 @@ class CommentForm(forms.ModelForm):
         fields = ['content']
 
 class CultureForm(forms.ModelForm):
+    start_time = forms.CharField(widget=forms.TextInput(attrs={'placeholder':' * 2019-01-01 형태로 입력해주세요.'}))
+    finish_time = forms.CharField(widget=forms.TextInput(attrs={'placeholder':' * 2019-01-01 형태로 입력해주세요.'}))
     class Meta:
         model = Culture
         fields = ['title', 'picture', 'start_time', 'finish_time', 'content', 'group']
