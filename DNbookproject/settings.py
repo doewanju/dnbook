872d18e65b55f3f0c3b932f2148634cca8649baa
ehdhkswn1997 <ehdhkswn@naver.com>
2019-11-28 16:08:11 +0900

    host

--- DNbookproject/settings.py
+++ DNbookproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l)_=05l01nmfun^$ymr=+fq!4zda-qim2@*6tnmss_26kh!04*'
 #SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
